b5a028039760818fd522e5d39da9605c00afc0b9
vg0x00 <vg0x00@gmail.com>
2018-05-26 10:44:52 +0800

    remove useless menu view stuff

--- Gank4Swifty/View/HomeViewController.swift
+++ Gank4Swifty/View/HomeViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var calendarBottomConstraint: NSLayoutConstraint!
-    @IBOutlet weak var menuTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var menuView: UIView! 
     @IBOutlet weak var calendarView: CalendarView!
     @IBOutlet weak var dimMask: UIView!
     @IBOutlet weak var tableView: UITableView!
@@ -34,35 +32,12 @@ class HomeViewController: UIViewController {
     var selectedModelItem: DataModel?
     var currentTask: URLSessionTask?
 
-    @IBAction func menuButtonTapped(_ sender: UIBarButtonItem) {
-        if calendarBottomConstraint.constant != 0 {
-            UIView.animate(withDuration: 0.12, delay: 0, options: .curveEaseOut, animations: {
-                self.animateCalendarIfNeeded(false)
-                self.view.layoutIfNeeded()
-            }) { (_) in
-                self.animateMenuIfNeeded(true)
-            }
-        } else {
-            animateMenuIfNeeded(true)
-        }
-    }
-
     @IBAction func calendarButtonTapped(_ sender: UIBarButtonItem) {
-        if menuTrailingConstraint.constant != 0 {
-            UIView.animate(withDuration: 0.12, delay: 0, options: .curveEaseOut, animations: {
-                self.animateMenuIfNeeded(false)
-                self.view.layoutIfNeeded()
-            }) { (_) in
-                self.animateCalendarIfNeeded(true)
-            }
-        } else {
             animateCalendarIfNeeded(true)
-        }
     }
 
     @IBAction func maskViewTapped(_ sender: UITapGestureRecognizer) {
         calendarBottomConstraint.constant = 0
-        menuTrailingConstraint.constant = 0
         UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseInOut, animations: {
             self.dimMask.alpha = 0
             self.view.layoutIfNeeded()
@@ -83,18 +58,6 @@ class HomeViewController: UIViewController {
         } else {
             calendarButton.image = UIImage(named: "icons8-overtime")
         }
-        if shouldAnimate {
-            UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseInOut, animations: {
-                self.dimMask.alpha = targetShadowMaskAlpha
-                self.view.layoutIfNeeded()
-            }, completion: nil)
-        }
-    }
-
-    private func animateMenuIfNeeded(_ shouldAnimate: Bool) {
-        let targetConstant: CGFloat = menuTrailingConstraint.constant == 0 ? 180 : 0
-        let targetShadowMaskAlpha: CGFloat = targetConstant == 0 ? 0 : 0.4
-        menuTrailingConstraint.constant = targetConstant
         if shouldAnimate {
             UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseInOut, animations: {
                 self.dimMask.alpha = targetShadowMaskAlpha
