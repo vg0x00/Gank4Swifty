af75c2b83692f3cc3dbd94bd32aee2d1e4866061
vg0x00 <vg0x00@gmail.com>
2018-06-01 15:10:37 +0800

    add push notification support

--- Gank4Swifty/AppDelegate.swift
+++ Gank4Swifty/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,31 +18,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.shared.statusBarStyle = .lightContent
+
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if error != nil || !granted {
+                print("debug: push notification feature disabled")
+            }
+            UIApplication.shared.registerForRemoteNotifications()
+        }
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        APIManager.shared.postDeivceTokenIfNeeded(token: token, failureHandler: nil, successHandler: nil)
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("failed to register notication with error: \(error)")
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print("debug received push notification")
+
+        guard let keyWindow = UIApplication.shared.keyWindow,
+            let rootTabbarController = keyWindow.rootViewController as? UITabBarController else { return }
+        rootTabbarController.selectedIndex = 0
+        guard let viewControllers = rootTabbarController.viewControllers,
+            let targetNavigationController = viewControllers[rootTabbarController.selectedIndex] as? UINavigationController else { return }
+        targetNavigationController.popToRootViewController(animated: true)
+        if let homeVC = targetNavigationController.viewControllers.first as? HomeViewController{
+            homeVC.fetchCalendarData()
+        }
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
